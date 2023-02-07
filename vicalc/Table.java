05bf3375f017b2d8c94006cf5d53e1aa2e1cff96
thomas <ulfljotsvatn930@gmail.com>
2023-02-06 22:58:53 -0500

    .

--- vicalc/Table.java
+++ vicalc/Table.java
@@ -5,6 +5,7 @@ public class Table {
 
     final int MAX_CELLS_ROW = 2^10;
     final int MAX_CELLS_COL = 2^12;
+    final int MAX_CELLS = MAX_CELLS_COL * MAX_CELLS_ROW;
 
-    Cell[][] nonEmptyCells = new Cell[MAX_CELLS_ROW][MAX_CELLS_COL];
+    Cell[] nonEmptyCells = new Cell[MAX_CELLS];
 }
