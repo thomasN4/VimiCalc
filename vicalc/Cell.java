0e7999c7731912616788abfac90c3a845d2b02e5
thomas <ulfljotsvatn930@gmail.com>
2023-02-06 22:56:28 -0500

    .

--- vicalc/Cell.java
+++ vicalc/Cell.java
@@ -4,4 +4,12 @@ public class Cell {
     int x_pos;
     int y_pos;
     String content;
+
+    Cell (int x_pos, int y_pos, String content) {
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+        this.content = content;
+    }
+
+    Cell() {}
 }
--- vicalc/Table.java
+++ vicalc/Table.java
@@ -2,4 +2,9 @@ package vicalc;
 
 // sort of represents a whole spreadsheet file
 public class Table {
+
+    final int MAX_CELLS_ROW = 2^10;
+    final int MAX_CELLS_COL = 2^12;
+
+    Cell[][] nonEmptyCells = new Cell[MAX_CELLS_ROW][MAX_CELLS_COL];
 }
