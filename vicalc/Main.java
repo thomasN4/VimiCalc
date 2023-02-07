104248b5337695b81d14487c4b3e7defd854972e
thomas <ulfljotsvatn930@gmail.com>
2023-02-06 23:09:01 -0500

    .

--- vicalc/Main.java
+++ vicalc/Main.java
@@ -1,9 +1,17 @@
 package vicalc;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
         Table newTable = new Table();
+    }
+
+    public static void save() throws FileNotFoundException {
+        String fileName = ".vicalc";
+        FileReader f = new FileReader(fileName);
     }
 
 }
