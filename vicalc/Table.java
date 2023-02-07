f65958c51bd255d8d8e9d9a3d13e114a573cfdbf
thomas <ulfljotsvatn930@gmail.com>
2023-02-06 23:06:17 -0500

    .

--- vicalc/Table.java
+++ vicalc/Table.java
@@ -1,11 +1,22 @@
 package vicalc;
 
-// sort of represents a whole spreadsheet file
 public class Table {
 
     final int MAX_CELLS_ROW = 2^10;
     final int MAX_CELLS_COL = 2^12;
     final int MAX_CELLS = MAX_CELLS_COL * MAX_CELLS_ROW;
 
-    Cell[] nonEmptyCells = new Cell[MAX_CELLS];
+
+    Cell[] nonEmptyCells;
+
+    Table() {
+        var nonEmptyCells = new Cell[MAX_CELLS];
+    }
+
+    Table(Cell[] fileContent) {
+        var nonEmptyCells = new Cell[MAX_CELLS];
+        for (int i = 0; fileContent[i] != null; i++) {
+            nonEmptyCells[i] = fileContent[i];
+        }
+    }
 }
