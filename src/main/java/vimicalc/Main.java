5f646faa9955f7d6c26671c1894104f9784ff6fe
thomas <ulfljotsvatn930@gmail.com>
2023-02-08 14:11:38 -0500

    .

--- src/main/java/vimicalc/Main.java
+++ src/main/java/vimicalc/Main.java
@@ -7,20 +7,19 @@ import javafx.stage.Stage;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("GUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-        stage.setTitle("VimiCalc");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("VimiCalc");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
     public static void save() throws IOException {
