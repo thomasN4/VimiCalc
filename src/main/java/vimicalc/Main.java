f0f134c455b3368ed84f3ff41f43ab6b9ac7827b
thomas <ulfljotsvatn930@gmail.com>
2023-02-08 13:44:07 -0500

    .

--- src/main/java/vimicalc/Main.java
+++ src/main/java/vimicalc/Main.java
@@ -1,17 +1,16 @@
 package vimicalc;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.FileWriter;
 import java.io.IOException;
 
-import static javafx.application.Application.launch;
 
-public class Main {
+public class Main extends Application {
 
+    @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("GUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
