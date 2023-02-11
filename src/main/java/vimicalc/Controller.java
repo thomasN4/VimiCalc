3aac0daf666336b3f1d79cc9df1526885f87a935
thomas <ulfljotsvatn930@gmail.com>
2023-02-11 15:31:40 -0500

    .

--- src/main/java/vimicalc/Controller.java
+++ src/main/java/vimicalc/Controller.java
@@ -6,8 +6,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.MenuBar;
 import javafx.scene.paint.*;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,46 +17,58 @@ public class Controller implements Initializable {
     @FXML
     private Canvas canvas;
 
-//    private Text letters, numbers;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
+    private void iBar_init() {
         GraphicsContext iBar = canvas.getGraphicsContext2D();
         iBar.setFill(Color.WHITE);
         int iBar_h = (int) Main.DEFAULT_CELL_HEIGHT;
         iBar.fillRect(0, canvas.getHeight()-iBar_h, canvas.getWidth(), iBar_h);
+    }
 
+    private void sBar_init() {
         GraphicsContext sBar = canvas.getGraphicsContext2D();
         sBar.setFill(Color.DARKGRAY);
         int sBar_h = (int) Main.DEFAULT_CELL_HEIGHT+4,
-            sBar_y = (int) canvas.getHeight()-sBar_h-iBar_h;
+            sBar_y = (int) (canvas.getHeight()-sBar_h-Main.DEFAULT_CELL_HEIGHT);
         sBar.fillRect(0, sBar_y, canvas.getWidth(), sBar_h);
-
-        GraphicsContext lRow = canvas.getGraphicsContext2D();
-        lRow.setFill(Color.LIGHTGRAY);
-        int lRow_h = (int) Main.DEFAULT_CELL_HEIGHT;
-        iBar.fillRect(0, 0, canvas.getWidth(), lRow_h);
-
-        GraphicsContext nCol = canvas.getGraphicsContext2D();
-        nCol.setFill(Color.LIGHTGRAY);
-        int nCol_w = (int) Main.DEFAULT_CELL_WIDTH;
-        iBar.fillRect(0, lRow_h, nCol_w, canvas.getHeight()-lRow_h-iBar_h-sBar_h);
-
-        GraphicsContext table = canvas.getGraphicsContext2D();
-        table.setFill(Color.WHITE);
-        table.fillRect(nCol_w, lRow_h, canvas.getWidth()-nCol_w, canvas.getHeight()-lRow_h-iBar_h-sBar_h);
-
-        GraphicsContext selCell = canvas.getGraphicsContext2D();
-        selCell.setFill(Color.LIGHTGRAY);
-        selCell.fillRect(nCol_w*2, lRow_h*2, nCol_w, lRow_h);
-
     }
 
-//    private void setLsNs() {
-//        Text new_letters = new Text(),
-//             new_numbers = new Text();
-//
-//    }
+    private void lRow_init() {
+        GraphicsContext lRow = canvas.getGraphicsContext2D();
+        lRow.setFill(Color.LIGHTGRAY);
+        lRow.fillRect(0, 0, canvas.getWidth(), Main.DEFAULT_CELL_HEIGHT);
+    }
+
+    private void nCol_init() {
+        GraphicsContext nCol = canvas.getGraphicsContext2D();
+        nCol.setFill(Color.LIGHTGRAY);
+        nCol.fillRect(0, Main.DEFAULT_CELL_HEIGHT, Main.DEFAULT_CELL_WIDTH, canvas.getHeight()-3*Main.DEFAULT_CELL_HEIGHT-4);
+    }
+
+    private void picture_init() {
+        GraphicsContext table = canvas.getGraphicsContext2D();
+        table.setFill(Main.DEFAULT_CELL_COLOR);
+        table.fillRect(Main.DEFAULT_CELL_WIDTH, Main.DEFAULT_CELL_HEIGHT, canvas.getWidth()-Main.DEFAULT_CELL_WIDTH, canvas.getHeight()-3*Main.DEFAULT_CELL_HEIGHT-4);
+    }
+
+    private void camera_init() {
+        ;
+    }
+
+    private void selCell_init() {
+        GraphicsContext selCell = canvas.getGraphicsContext2D();
+        selCell.setFill(Color.LIGHTGRAY);
+        selCell.fillRect(2*Main.DEFAULT_CELL_WIDTH, 2*Main.DEFAULT_CELL_HEIGHT, Main.DEFAULT_CELL_WIDTH, Main.DEFAULT_CELL_HEIGHT);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        iBar_init();
+        sBar_init();
+        lRow_init();
+        nCol_init();
+        picture_init();
+        camera_init();
+        selCell_init();
+    }
 
 }
