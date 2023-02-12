42c93beae3b952624053e2c036faf66d210fb282
thomas <ulfljotsvatn930@gmail.com>
2023-02-12 01:06:59 -0500

    .

--- src/main/java/vimicalc/controller/Controller.java
+++ src/main/java/vimicalc/controller/Controller.java
@@ -6,16 +6,15 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.MenuBar;
 import javafx.scene.paint.*;
-import javafx.scene.shape.Rectangle;
 import vimicalc.view.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    private final int DEFAULT_CELL_HEIGHT = 23;
-    private final int DEFAULT_CELL_WIDTH = DEFAULT_CELL_HEIGHT*4;
-    private final Color DEFAULT_CELL_COLOR = Color.WHITE;
+    final int DEFAULT_CELL_HEIGHT = 23;
+    final int DEFAULT_CELL_WIDTH = DEFAULT_CELL_HEIGHT*4;
+    final Color DEFAULT_CELL_COLOR = Color.WHITE;
 
     @FXML private MenuBar menuBar;
     @FXML private Canvas canvas;
@@ -38,9 +37,12 @@ public class Controller implements Initializable {
         infoBar = new InfoBar(0, (int) canvas.getHeight()-DEFAULT_CELL_HEIGHT, canvas.getWidth(), DEFAULT_CELL_HEIGHT, DEFAULT_CELL_COLOR);
         statusBar = new StatusBar(0, (int) (canvas.getHeight()-2*DEFAULT_CELL_HEIGHT-4), canvas.getWidth(), DEFAULT_CELL_HEIGHT+4, Color.DARKGRAY);
         selectedCell = new SelectedCell(2*DEFAULT_CELL_WIDTH, 2*DEFAULT_CELL_HEIGHT, DEFAULT_CELL_WIDTH, DEFAULT_CELL_HEIGHT, Color.LIGHTGRAY);
-    }
 
-    public GraphicsContext getGc() {
-        return gc;
+        camera.getPicture().draw(gc);
+        firstCol.draw(gc);
+        firstRow.draw(gc);
+        infoBar.draw(gc);
+        statusBar.draw(gc);
+        selectedCell.draw(gc);
     }
 }
