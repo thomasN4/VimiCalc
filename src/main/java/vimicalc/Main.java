2a151d472012a8e9e70e3c859df98e0dd46f0e95
thomas <ulfljotsvatn930@gmail.com>
2023-02-11 20:51:43 -0500

    .

--- src/main/java/vimicalc/Main.java
+++ src/main/java/vimicalc/Main.java
@@ -3,16 +3,11 @@ package vimicalc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
-
-    static final double DEFAULT_CELL_HEIGHT = 23;
-    static final double DEFAULT_CELL_WIDTH = DEFAULT_CELL_HEIGHT*4;
-    static final Color DEFAULT_CELL_COLOR = Color.WHITE;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
