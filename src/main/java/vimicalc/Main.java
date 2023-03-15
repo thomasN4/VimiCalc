54cd0819cd65b3a0b14971fdb9ba9128bb4ef769
SPAC3-M0NK3Y <80297768+SPAC3-M0NK3Y@users.noreply.github.com>
2023-03-14 23:26:38 -0400

    Ajout ScrollBars

--- src/main/java/vimicalc/Main.java
+++ src/main/java/vimicalc/Main.java
@@ -2,7 +2,11 @@ package vimicalc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollBar;
 import javafx.stage.Stage;
 import vimicalc.controller.Controller;
 
@@ -13,8 +17,32 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("GUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+
+        //CD: Scrollbar Horizontal & ses parameters (to-do fix avec camera)
+        ScrollBar bar1 = new ScrollBar();
+        bar1.setLayoutX(0);
+        bar1.setLayoutY(478);
+        bar1.setMin(0);
+        bar1.setMax(100);
+        bar1.setValue(1);
+        bar1.setOrientation(Orientation.HORIZONTAL);
+        bar1.setPrefSize(640,10);
+
+        //CD: Scrollbar Vertical & ses parameters (to-do fix avec camera)
+        ScrollBar bar2 = new ScrollBar();
+        bar2.setLayoutX(640);
+        bar2.setLayoutY(0);
+        bar2.setMin(0);
+        bar2.setMax(100);
+        bar2.setValue(1);
+        bar2.setOrientation(Orientation.VERTICAL);
+        bar2.setPrefSize(10,478);
+
+        Group root = new Group((Node) fxmlLoader.load(), bar1, bar2); //CD: regroupe le fxml et les scrollbars
+
+        Scene scene = new Scene(root, 640, 480); //CD: Modif (root)
         scene.setOnKeyPressed(Controller::onKeyPressed);
+        //scene.setOnMouseClicked(Controller::onMouseClicked); //CD: pour les cliques avec souris
         primaryStage.setTitle("VimiCalc");
         primaryStage.setScene(scene);
         primaryStage.show();
