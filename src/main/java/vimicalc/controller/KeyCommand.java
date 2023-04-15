70fa2b4e6b144aabe8fc688e4b2e6e695fcd6bc0
Thomas Nguyen <ulfljotsvatn930@gmail.com>
2023-04-15 18:13:09 -0400

    .

--- src/main/java/vimicalc/controller/KeyCommand.java
+++ src/main/java/vimicalc/controller/KeyCommand.java
@@ -15,9 +15,9 @@ import static vimicalc.utils.Conversions.isNumber;
 public class KeyCommand {
 
     // Les fonctions A sont des fonctions avec des arguments, mais les fonctions B, non
-    private final char[] Afuncs = {'d', 'y', 'p'};
+//    private final char[] Afuncs = {'d', 'y', 'p'};
     private final char[] Bfuncs = {'h', 'j', 'k', 'l', 'a', 'i'};
-    private HashMap<Character, String> macros;
+//    private HashMap<Character, String> macros;
     private String expr;
 
     public KeyCommand() {
@@ -76,55 +76,71 @@ public class KeyCommand {
     }
 
     public void evaluate() {
-        switch (expr.charAt(0)) {
-            case 'h' -> moveLeft();
-            case 'j' -> moveDown();
-            case 'k' -> moveUp();
-            case 'l' -> moveRight();
-            case 'd' -> {
-                if (cellSelector.getSelectedCell().txt() == null)
-                    infoBar.setInfobarTxt("CAN'T DELETE RIGHT NOW");
-                else {
-                    recordedCell.add(cellSelector.getSelectedCell());
-                    sheet.deleteCell(coordsCell.getCoords());
+        byte firstFuncIndex = 0;
+        int multiplier = 1;
+        String multiplierStr = "";
+        for (int i = 0; i < expr.length(); i++) {
+            if (isNumber(""+expr.charAt(i))) {
+                multiplierStr += expr.charAt(i);
+                ++firstFuncIndex;
+            }
+            else {
+                if (firstFuncIndex != 0)
+                    multiplier = Integer.parseInt(multiplierStr);
+                break;
+            }
+        }
+        for (int i = 0; i < multiplier; i++) {
+            switch (expr.charAt(firstFuncIndex)) {
+                case 'h' -> moveLeft();
+                case 'j' -> moveDown();
+                case 'k' -> moveUp();
+                case 'l' -> moveRight();
+                case 'd' -> {
+                    if (cellSelector.getSelectedCell().txt() == null)
+                        infoBar.setInfobarTxt("CAN'T DELETE RIGHT NOW");
+                    else {
+                        recordedCell.add(cellSelector.getSelectedCell());
+                        sheet.deleteCell(coordsCell.getCoords());
+                        camera.picture.take(gc, sheet, selectedCoords, camera.getAbsX(), camera.getAbsY());
+                        camera.ready();
+                        cellSelector.readCell(camera.picture.data());
+                        recordedCell.add(cellSelector.getSelectedCell().copy());
+                        infoBar.setInfobarTxt(cellSelector.getSelectedCell().txt());
+                    }
+                }
+                case 'm' -> sheet.unmergeCells(sheet.findCell(coordsCell.getCoords()));
+                case 'u' -> {
+                    if (!recordedCell.isEmpty() && !(dCounter >= recordedCell.size())) undo();
+                    else infoBar.setInfobarTxt("CAN'T UNDO RIGHT NOW");
                     camera.picture.take(gc, sheet, selectedCoords, camera.getAbsX(), camera.getAbsY());
                     camera.ready();
                     cellSelector.readCell(camera.picture.data());
-                    recordedCell.add(cellSelector.getSelectedCell().copy());
-                    infoBar.setInfobarTxt(cellSelector.getSelectedCell().txt());
                 }
-            }
-            case 'm' -> sheet.unmergeCells(sheet.findCell(coordsCell.getCoords()));
-            case 'u' -> {
-                if (!recordedCell.isEmpty() && !(dCounter >= recordedCell.size())) undo();
-                else infoBar.setInfobarTxt("CAN'T UNDO RIGHT NOW");
-                camera.picture.take(gc, sheet, selectedCoords, camera.getAbsX(), camera.getAbsY());
-                camera.ready();
-                cellSelector.readCell(camera.picture.data());
-            }
-            case 'r' -> {
-                if (!recordedCell.isEmpty() && !(dCounter <= 1)) redo();
-                else infoBar.setInfobarTxt("CAN'T REDO RIGHT NOW");
-                camera.picture.take(gc, sheet, selectedCoords, camera.getAbsX(), camera.getAbsY());
-                camera.ready();
-                cellSelector.readCell(camera.picture.data());
-            }
-            case 'i' -> {
-                cellSelector.setSelectedCell(new Cell(
-                    cellSelector.getXCoord(),
-                    cellSelector.getYCoord(),
-                    cellSelector.getSelectedCell().txt()
-                ));
-                recordedCell.add(cellSelector.getSelectedCell().copy());
-                cellSelector.readCell(camera.picture.data());
-                currMode = Mode.INSERT;
-                if (cellSelector.getSelectedCell().txt() == null)
-                    cellSelector.getSelectedCell().setTxt("");
-                cellSelector.draw(gc);
-            }
-            case 'v' -> {
-                currMode = Mode.VISUAL;
-                selectedCoords.add(new int[]{cellSelector.getXCoord(), cellSelector.getYCoord()});
+                case 'r' -> {
+                    if (!recordedCell.isEmpty() && !(dCounter <= 1)) redo();
+                    else infoBar.setInfobarTxt("CAN'T REDO RIGHT NOW");
+                    camera.picture.take(gc, sheet, selectedCoords, camera.getAbsX(), camera.getAbsY());
+                    camera.ready();
+                    cellSelector.readCell(camera.picture.data());
+                }
+                case 'i' -> {
+                    cellSelector.setSelectedCell(new Cell(
+                            cellSelector.getXCoord(),
+                            cellSelector.getYCoord(),
+                            cellSelector.getSelectedCell().txt()
+                    ));
+                    recordedCell.add(cellSelector.getSelectedCell().copy());
+                    cellSelector.readCell(camera.picture.data());
+                    currMode = Mode.INSERT;
+                    if (cellSelector.getSelectedCell().txt() == null)
+                        cellSelector.getSelectedCell().setTxt("");
+                    cellSelector.draw(gc);
+                }
+                case 'v' -> {
+                    currMode = Mode.VISUAL;
+                    selectedCoords.add(new int[]{cellSelector.getXCoord(), cellSelector.getYCoord()});
+                }
             }
         }
         expr = "";
