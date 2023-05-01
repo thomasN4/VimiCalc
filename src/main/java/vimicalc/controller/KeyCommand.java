39a7620ec3f028dfbb43f51b9409390aecef01dc
Thomas Nguyen <ulfljotsvatn930@gmail.com>
2023-05-01 00:13:19 -0400

    .

--- src/main/java/vimicalc/controller/KeyCommand.java
+++ src/main/java/vimicalc/controller/KeyCommand.java
@@ -262,7 +262,6 @@ public class KeyCommand {
                             camera.picture.take(gc, sheet, selectedCoords, camera.getAbsX(), camera.getAbsY());
                             camera.ready();
                             cellSelector.readCell(camera.picture.data());
-                            recordedCellStates.add(cellSelector.getSelectedCell().copy());
                             infoBar.setInfobarTxt(cellSelector.getSelectedCell().txt());
                         }
                         this.expr = "";
