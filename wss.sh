b29a4e80b5d07ca098c38c889a6f8abe3cc02214
thomas <ulfljotsvatn930@gmail.com>
2023-05-16 22:17:50 -0400

    .

--- wss.sh
+++ wss.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 ## Modifiez la location du dossier lib de votre installation de JavaFX 17 et du fichier .jar s'il vous plaît.
-# shellcheck disable=SC2086
 java --module-path "/usr/lib/jvm/javafx-sdk-17.0.7/lib" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -jar /opt/VimiCalc-main/out/artifacts/VimiCalc_jar/VimiCalc.jar $1
