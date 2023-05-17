ae0f1fe72e6aab3365c352aa27f75416bb29097c
Thomas Nguyen <ulfljotsvatn930@gmail.com>
2023-05-16 23:20:46 -0400

    .

--- wss.ps1
+++ wss.ps1
@@ -1,1 +1,2 @@
+## Modifiez la location du dossier lib de votre installation de JavaFX 17 et du fichier .jar s'il vous plaît.
 java --module-path "PATH_TO_LIBRARY_(JAVA 17)" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -jar PATH_TO_JAR $1
